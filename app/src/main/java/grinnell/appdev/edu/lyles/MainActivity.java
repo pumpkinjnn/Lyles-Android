7d968a23f55acd439ec5c956855000d817ba61c6
Mattori Birnbaum <mattori.birnbaum@gmail.com>
2016-05-09 20:23:03 -0500

    added drawer, toolbar, and initial schedule fragment base

--- app/src/main/java/grinnell/appdev/edu/lyles/MainActivity.java
+++ app/src/main/java/grinnell/appdev/edu/lyles/MainActivity.java
@@ -1,7 +1,17 @@
 package grinnell.appdev.edu.lyles;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import grinnell.appdev.edu.lyles.fragments.ScheduleFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
+
+        NavigationView nav = (NavigationView) findViewById(R.id.nav_view);
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                if(item.isChecked()) item.setChecked(false);
+                else item.setChecked(true);
+
+                drawer.closeDrawers();
+
+                Fragment newFrag;
+
+                switch(item.getItemId()) {
+                    case R.id.schedule:
+                        Log.d("nav", "Schedule selected from nav");
+                        newFrag = new ScheduleFragment();
+                        break;
+                    default:
+                        Log.d("nav", "Invalid nav option selected (" + item.getItemId() + ")");
+                        Toast.makeText(getApplicationContext(), "Invalid nav option selected (" + item.getItemId() + ")", Toast.LENGTH_LONG).show();
+                        return false;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame, newFrag).commit();
+                return true;
+            }
+        });
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open_drawer, R.string.close_drawer);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
