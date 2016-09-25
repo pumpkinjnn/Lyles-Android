8a73d72145ba268c5d13c0385db6aea07b83bdb6
murphymatt <murphyma@grinnell.edu>
2016-09-25 14:22:43 -0500

    removed rating field

--- app/src/main/java/grinnell/appdev/edu/lyles/menuItems/BeerItem.java
+++ app/src/main/java/grinnell/appdev/edu/lyles/menuItems/BeerItem.java
@@ -7,14 +7,10 @@ public class BeerItem {
 
     private String mName;
     private String mDescription;
-    private int mRating;
-
-
 
     public BeerItem() {
         mName = null;
         mDescription = null;
-        mRating = -1;
     }
 
     public String getName() {
@@ -25,19 +21,11 @@ public class BeerItem {
         return mDescription;
     }
 
-    public int getRating() {
-        return mRating;
-    }
-
     public void setName(String name) {
         mName = name;
     }
 
     public void setDescription(String description) {
         mDescription = description;
-    }
-
-    public void setRating(int rating) {
-        mRating = rating;
     }
 }
