c2eb23fdc68df43aaf78d693586017d03b132ddd
murphymatt <murphyma@grinnell.edu>
2016-09-25 14:52:30 -0500

    Created MenuItem interface

--- app/src/main/java/grinnell/appdev/edu/lyles/menuItems/BeerItem.java
+++ app/src/main/java/grinnell/appdev/edu/lyles/menuItems/BeerItem.java
@@ -3,8 +3,7 @@ package grinnell.appdev.edu.lyles.menuItems;
 /**
  * Model beer item. Contains information on beer name and beer description
  */
-public class BeerItem {
-
+public class BeerItem implements MenuItem {
 
     // TODO: gather image resources from beer name
     // completed from switch statement on possible names
--- app/src/main/java/grinnell/appdev/edu/lyles/menuItems/MenuItem.java
+++ app/src/main/java/grinnell/appdev/edu/lyles/menuItems/MenuItem.java
@@ -4,40 +4,18 @@ package grinnell.appdev.edu.lyles.menuItems;
  * Created by mattmurphy on 9/25/16.
  */
 
-public class MenuItem {
+public interface MenuItem {
 
-    private String mName;
-    private String mDescription;
-    private double mPrice;
+    public String getName();
 
-    public MenuItem() {
-        mName = null;
-        mDescription = null;
-        mPrice = 0.00;
-    }
+    public String getDescription();
 
-    public String getName() {
-        return mName;
-    }
+    public void setName(String name);
 
-    public String getDescription() {
-        return mDescription;
-    }
+    public void setDescription(String description);
 
-    public void setName(String name) {
-        mName = name;
-    }
+    public double getPrice();
 
-    public void setDescription(String description) {
-        mDescription = description;
-    }
-
-    public double getPrice() {
-        return mPrice;
-    }
-
-    public void setPrice(double price) {
-        mPrice = price;
-    }
+    public void setPrice(double price);
 
 }
