194c79dad52fa2a4ca59d79ea29fec2f07e13fa6
murphymatt <murphyma@grinnell.edu>
2016-09-25 14:15:24 -0500

    filled out BeerItem model

--- app/src/main/java/grinnell/appdev/edu/lyles/menuItems/BeerItem.java
+++ app/src/main/java/grinnell/appdev/edu/lyles/menuItems/BeerItem.java
@@ -1,8 +1,43 @@
 package grinnell.appdev.edu.lyles.menuItems;
 
 /**
- * Created by mattmurphy on 9/25/16.
+ * Model reores
  */
-
 public class BeerItem {
+
+    /**
+     * TODO:
+     * fill out fields, constructor, and methods
+     *
+     * fields:
+     * name and description of beer
+     *
+     * methods:
+     * field getters
+     *
+     */
+
+    private String mName;
+    private String mDescription;
+
+    public BeerItem() {
+        mName = null;
+        mDescription = null;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
 }
