d6666c7856f6a6b1ba5c46a38b69ee73dc58ec2e
murphymatt <murphyma@grinnell.edu>
2016-09-25 14:53:33 -0500

    Added method implementations

--- app/src/main/java/grinnell/appdev/edu/lyles/menuItems/BeerItem.java
+++ app/src/main/java/grinnell/appdev/edu/lyles/menuItems/BeerItem.java
@@ -5,6 +5,36 @@ package grinnell.appdev.edu.lyles.menuItems;
  */
 public class BeerItem implements MenuItem {
 
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+
+    @Override
+    public void setDescription(String description) {
+
+    }
+
+    @Override
+    public double getPrice() {
+        return 0;
+    }
+
+    @Override
+    public void setPrice(double price) {
+
+    }
+
     // TODO: gather image resources from beer name
     // completed from switch statement on possible names
     public String getImageResource() {
