84ce36b1542d66d3d35811a38368b80c782d4f2a
murphymatt <murphyma@grinnell.edu>
2016-10-02 14:06:25 -0500

    set adapter fields to final

--- app/src/main/java/grinnell/appdev/edu/lyles/Adapters/BeerAdapter.java
+++ app/src/main/java/grinnell/appdev/edu/lyles/Adapters/BeerAdapter.java
@@ -1,18 +1,14 @@
 package grinnell.appdev.edu.lyles.adapters;
 
 import android.content.Context;
-import android.graphics.Picture;
-import android.hardware.camera2.params.StreamConfigurationMap;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import grinnell.appdev.edu.lyles.MainActivity;
 import grinnell.appdev.edu.lyles.R;
 import grinnell.appdev.edu.lyles.menuItems.BeerItem;
 
@@ -23,7 +19,6 @@ import grinnell.appdev.edu.lyles.menuItems.BeerItem;
  * adapter contains image, and description of catelog item
  */
 public class BeerAdapter extends ArrayAdapter<BeerItem> {
-    private static final String TAG = ArrayAdapter.class.getSimpleName();
 
     public BeerAdapter(Context context, int resource) {
         super(context, resource);
@@ -39,21 +34,21 @@ public class BeerAdapter extends ArrayAdapter<BeerItem> {
 
     public View getView(int pos, View convertView, ViewGroup parent) {
         if (convertView == null) {
-            convertView = LayoutInflater.from(this.getContext()).inflate(R.layout.beer_list_item, parent, false);
+            convertView = LayoutInflater.from(this.getContext())
+                    .inflate(R.layout.beer_list_item, parent, false);
         }
 
         BeerItem beer = BeerAdapter.super.getItem(pos);
 
         if (beer != null) {
-            Log.d("beer", "Beer item is not null");
-            ImageView beerImage = (ImageView) convertView.findViewById(R.id.beer_image);
+            final ImageView beerImage = (ImageView) convertView.findViewById(R.id.beer_image);
             // TODO: figure out how to set beer images
             // beerImage.setImageDrawable((drawable) beer.getImageResource());
 
-            TextView  beerName  = (TextView)  convertView.findViewById(R.id.beer_name);
+            final TextView  beerName  = (TextView)  convertView.findViewById(R.id.beer_name);
             beerName.setText(beer.getName());
 
-            TextView  beerType  = (TextView)  convertView.findViewById(R.id.beer_type);
+            final TextView  beerType  = (TextView)  convertView.findViewById(R.id.beer_type);
             beerType.setText(beer.getDescription());
         }
 
