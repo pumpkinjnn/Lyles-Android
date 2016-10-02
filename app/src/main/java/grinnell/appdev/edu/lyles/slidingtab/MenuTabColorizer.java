a025ff40d1f3ff5e7ff685e9310788c42e3d63a8
Mattori Birnbaum <mattori.birnbaum@gmail.com>
2016-10-02 13:54:54 -0500

    created colorizer for menu tabs

---
+++ app/src/main/java/grinnell/appdev/edu/lyles/slidingtab/MenuTabColorizer.java
@@ -0,0 +1,24 @@
+package grinnell.appdev.edu.lyles.slidingtab;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+
+import grinnell.appdev.edu.lyles.R;
+
+/**
+ * Created by Mattori on 9/29/16.
+ */
+
+public class MenuTabColorizer implements SlidingTabLayout.TabColorizer {
+
+    private Context mContext;
+
+    public MenuTabColorizer(Context ctxt) {
+        mContext = ctxt;
+    }
+
+    @Override
+    public int getIndicatorColor(int pos) {
+        return ContextCompat.getColor(mContext, R.color.tabsScrollColor);
+    }
+}
