d17109123085b94eef319a270c2823d65faf617d
Mattori Birnbaum <mattori.birnbaum@gmail.com>
2016-10-03 23:06:00 -0500

    added missing calls and removed old unused code

--- app/src/main/java/grinnell/appdev/edu/lyles/fragments/MenuFragment.java
+++ app/src/main/java/grinnell/appdev/edu/lyles/fragments/MenuFragment.java
@@ -6,6 +6,7 @@ import android.support.design.widget.TabItem;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTabHost;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.Choreographer;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -37,20 +38,8 @@ public class MenuFragment extends Fragment {
         View view = inflater.inflate(R.layout.menu_layout, container, false);
 
         setupViewPager(view);
-
-        // create and setup tabHost
-        menuHost = (FragmentTabHost) view.findViewById(android.R.id.tabhost);
-        menuHost.setup(getActivity(), getChildFragmentManager(), android.R.id.tabcontent);
-
-        // create tabs and add to menuHost
-        addToTabHost("Hot Food",  1, HotFoodFragment.class);
-        addToTabHost("Snacks",    2, SnacksFragment.class);
-        addToTabHost("Drinks",    3, DrinksFragment.class);
-        addToTabHost("Favorites", 4, FavoriteFoodFragment.class);
-
-        //TODO: change text size in tab views
-
-        return menuHost;
+        
+        return view;
     }
 
     @Override
@@ -58,20 +47,15 @@ public class MenuFragment extends Fragment {
         super.onCreateOptionsMenu(menu, inflater);
     }
 
-    private void addToTabHost(String tabName, int num, Class fragmentClass) {
-        Bundle bundle = new Bundle();
-        bundle.putInt(tabName + " Tab", num);
-        menuHost.addTab(menuHost.newTabSpec(tabName).setIndicator(tabName),
-                fragmentClass, bundle);
-    }
-
     private void setupViewPager(View view) {
-        MenuViewPagerAdapter adapter = new MenuViewPagerAdapter(getFragmentManager());
+        MenuViewPagerAdapter adapter = new MenuViewPagerAdapter(getFragmentManager(), getContext());
         ViewPager pager = (ViewPager) view.findViewById(R.id.pager);
         pager.setAdapter(adapter);
 
         SlidingTabLayout tabs = (SlidingTabLayout) view.findViewById(R.id.tabs);
         tabs.setDistributeEvenly(true);
         tabs.setCustomTabColorizer(new MenuTabColorizer(getContext()));
+
+        tabs.setViewPager(pager);
     }
 }
