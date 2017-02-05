e1458b3d69b25e6ba5d51095a19f85d4a1d974d6
zeb <z.figura12@gmail.com>
2017-02-05 14:03:26 -0600

    added image

--- app/src/main/java/grinnell/appdev/edu/lyles/MenuTabListAdapter.java
+++ app/src/main/java/grinnell/appdev/edu/lyles/MenuTabListAdapter.java
@@ -1,16 +1,22 @@
 package grinnell.appdev.edu.lyles;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 
 /**
@@ -31,14 +37,16 @@ public class MenuTabListAdapter extends ArrayAdapter<JSONObject> {
         TextView title = (TextView) view.findViewById(R.id.menutab_title);
         TextView price = (TextView) view.findViewById(R.id.menutab_price);
         TextView details = (TextView) view.findViewById(R.id.menutab_details);
+        ImageView image = (ImageView) view.findViewById(R.id.menutab_image);
 
         try {
             title.setText(item.getString("title"));
             price.setText(item.getString("price"));
             details.setText(item.getString("details"));
-        } catch (JSONException e) {
+            image.setImageBitmap(BitmapFactory.decodeStream(new java.net.URL(item.getString("image")).openStream()));
+        } catch (IOException | JSONException e) {
             Log.d("error",e.getMessage());
-        }
+;        }
         return view;
     }
 }
