9b07afac76147877848d639e75462b7a83b438bf
pumpkinjnn <jinannan@grinnell.edu>
2017-02-12 14:21:26 -0600

    implementing beer fragment

--- app/src/main/java/grinnell/appdev/edu/lyles/fragments/BeerFragment.java
+++ app/src/main/java/grinnell/appdev/edu/lyles/fragments/BeerFragment.java
@@ -1,22 +1,117 @@
 package grinnell.appdev.edu.lyles.fragments;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 import grinnell.appdev.edu.lyles.R;
 
-/**
- * Created by Mattori on 5/9/16.
- */
-public class BeerFragment extends Fragment {
+public class BeerFragment extends AppCompatActivity {
+
+    ListView list;
+    BeersAdaptor adaptor;
+    ArrayList<Beers> beersList;
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.beer_layout, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.beer_layout);
+
+        list = (ListView) findViewById(R.id.lvBeers);
+        beersList = new ArrayList<Beers>();
+
+        new BeersAsynTask().execute("http://www.cs.grinnell.edu/~birnbaum/appdev/lyles/beer.json");
+
     }
 
+
+
+    public class BeersAsynTask extends AsyncTask<String, Void, Boolean> {
+
+        HttpURLConnection urlConnection;
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+            StringBuilder result = new StringBuilder();
+
+            try {
+                URL url = new URL("http://www.cs.grinnell.edu/~birnbaum/appdev/lyles/beer.json");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    result.append(line);
+                }
+
+            }catch( Exception e) {
+                e.printStackTrace();
+            }
+            finally {
+                urlConnection.disconnect();
+            }
+
+            String data = result.toString();
+            try  {
+                JSONObject beerInformation = new JSONObject(data);
+                JSONArray beers = beerInformation.getJSONArray("beer");
+
+                for (int i=0;i<beers.length();i++){
+                    Beers beer = new Beers();
+
+                    JSONObject aBeer = beers.getJSONObject(i);
+
+                    beer.setTitle(aBeer.getString("title"));
+                    beer.setSubtitle(aBeer.getString("subtitle"));
+                    beer.setPrice(aBeer.getDouble("price"));
+                    beer.setImage(aBeer.getString("image"));
+                    beer.setDetails(aBeer.getString("details"));
+
+                    beersList.add(beer);
+
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            super.onPostExecute(result);
+
+            if(!result){
+                // print message
+            }else {
+                BeersAdaptor adaptor = new BeersAdaptor(getApplicationContext(),R.layout.beer_row,beersList);
+                list.setAdapter(adaptor);
+
+            }
+        }
+    }
+
+
 }
+
+
+
+
